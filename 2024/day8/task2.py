9586471e88327b427232e95329a645e5aeda414b
Aditya Patel <adityakdpatel@gmail.com>
2025-03-07 12:50:27 -0500

    feat: solved day8 part 2 execution time: 0.110917 seconds

--- 2024/day8/task2.py
+++ 2024/day8/task2.py
@@ -21,18 +21,29 @@ def valid_antinode(position, row_count, col_count):
     return False
 
 
+def is_in_line(pos1, pos2, point):
+    (x1, y1) = pos1
+    (x2, y2) = pos2
+    (x3, y3) = point
+    return (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) == 0
+
+
 def get_antonode_positions(positions, row_count, col_count):
     antinodes = set()
-    for i in range(len(positions) - 1):
-        for j in range(i, len(positions)):
-            pos1 = positions[i]
-            pos2 = positions[j]
-            ant1 = (2 * pos1[0] - pos2[0], 2 * pos1[1] - pos2[1])
-            ant2 = (2 * pos2[0] - pos1[0], 2 * pos2[1] - pos1[1])
-            if valid_antinode(ant1, row_count, col_count) and ant1 not in positions:
-                antinodes.add(ant1)
-            if valid_antinode(ant2, row_count, col_count) and ant2 not in positions:
-                antinodes.add(ant2)
+    for pi in range(len(positions) - 1):
+        for pj in range(pi + 1, len(positions)):
+            pos1 = positions[pi]
+            pos2 = positions[pj]
+            for i in range(row_count):
+                for j in range(col_count):
+                    if pos1[1] == pos2[1]:
+                        if i <= pos1[0] or i >= pos2[0]:
+                            if is_in_line(pos1, pos2, (i, j)):
+                                antinodes.add((i, j))
+                    else:
+                        if j <= pos1[1] or j >= pos2[1]:
+                            if is_in_line(pos1, pos2, (i, j)):
+                                antinodes.add((i, j))
     return antinodes
 
 
