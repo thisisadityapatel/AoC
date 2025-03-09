2f8ea23f1b28859e0ed36174390445264db46c23
Aditya Patel <adityakdpatel@gmail.com>
2025-03-09 13:10:51 -0400

    feat: minor changes

--- 2024/day6/task2.py
+++ 2024/day6/task2.py
@@ -21,15 +21,19 @@ def has_cycle(gridmap):
 
     if not head:
         return False
-    
+
     while 0 < head[0] < row_length - 1 and 0 < head[1] < col_length - 1:
-        if (gridmap[head[0] + mapping[direction][0]][head[1] + mapping[direction][1]] == "#"):
+        if (
+            gridmap[head[0] + mapping[direction][0]][head[1] + mapping[direction][1]]
+            == "#"
+        ):
             direction = (direction + 1) % 4
         head = (head[0] + mapping[direction][0], head[1] + mapping[direction][1])
         if (head[0], head[1], direction) in positions:
             return True
         positions.add((head[0], head[1], direction))
     return False
+
 
 def get_path_positions(gridmap):
     positions = set()
@@ -48,31 +52,33 @@ def get_path_positions(gridmap):
             break
 
     while 0 < head[0] < row_length - 1 and 0 < head[1] < col_length - 1:
-        if gridmap[head[0] + mapping[direction][0]][head[1] + mapping[direction][1]] == "#":
+        if (
+            gridmap[head[0] + mapping[direction][0]][head[1] + mapping[direction][1]]
+            == "#"
+        ):
             direction = (direction + 1) % 4
         head = (head[0] + mapping[direction][0], head[1] + mapping[direction][1])
         positions.add(head)
     return positions
+
 
 def print_grid(gridmap):
     for line in gridmap:
         print("".join(line))
     print()
 
+
 if __name__ == "__main__":
     start_time = time.time()
-    grid = [list(line) for line in open('data.txt').read().splitlines()]
+    grid = [list(line) for line in open("data.txt").read().splitlines()]
     positions = get_path_positions(gridmap=grid)
     count = 0
     for pos in positions:
-        initial_value =  grid[pos[0]][pos[1]]
+        initial_value = grid[pos[0]][pos[1]]
         grid[pos[0]][pos[1]] = "#"
         if has_cycle(gridmap=grid):
-            count += 1    
+            count += 1
         grid[pos[0]][pos[1]] = initial_value
     end_time = time.time()
     print(count)
     print(f"execution time: {end_time - start_time:.6f} seconds")
-
-
-
