ccfc2d73b789c3e513279c83d66268f99adf99af
Aditya Patel <adityakdpatel@gmail.com>
2025-03-09 13:04:26 -0400

    feat: solved day10 part 1 execution time: 0.013440 seconds

---
+++ 2024/day10/task1.py
@@ -0,0 +1,46 @@
+import time
+
+
+def get_map(filename):
+    return [list(map(int, list(line))) for line in open(filename).read().splitlines()]
+
+
+def get_trail_count(pos, map):
+    row, col = len(map), len(map[0])
+    final_point_tracker = set()
+
+    def traversal(pos):
+        if map[pos[0]][pos[1]] == 9 and (pos[0], pos[1]) not in final_point_tracker:
+            final_point_tracker.add((pos[0], pos[1]))
+            return 1
+        count = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if abs(i + j) == 1:
+                    if (
+                        -1 < pos[0] + i < row
+                        and -1 < pos[1] + j < col
+                        and map[pos[0] + i][pos[1] + j] - map[pos[0]][pos[1]] == 1
+                    ):
+                        count += traversal((pos[0] + i, pos[1] + j))
+        return count
+
+    return traversal(pos)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    map = get_map("data.txt")
+    starting_points = set()
+    for i in range(len(map)):
+        for j in range(len(map)):
+            if map[i][j] == 0:
+                starting_points.add((i, j))
+
+    trail_count = 0
+    for start in starting_points:
+        trail_count += get_trail_count(start, map)
+
+    print(trail_count)
+    end_time = time.time()
+    print(f"execution time: {(end_time - start_time):.6f} seconds")
