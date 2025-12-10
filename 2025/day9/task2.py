76b953b72ec74ca6d876a6a36b05de9474f795e0
Aditya Kamleshkumar Patel <aditya.patel@wealthsimple.com>
2025-12-10 15:17:16 -0500

    part2

--- 2025/day9/task2.py
+++ 2025/day9/task2.py
@@ -63,22 +63,33 @@ def diagonal_red_tile_positions(points):
     )
 
 
+def solver(combinations, edges) -> int:
+    max_area = 0
+    for combination in combinations:
+        p1, p2 = combination
+        x1, y1 = p1
+        x2, y2 = p2
+        xmin, xmax = min(x1, x2), max(x1, x2)
+        ymin, ymax = min(y1, y2), max(y1, y2)
+        width = abs(x2 - x1) + 1
+        height = abs(y2 - y1) + 1
+        current_area = width * height
+        flag = True
+        for edge in edges:
+            if (xmin < edge[0] < xmax) and (ymin < edge[1] < ymax):
+                flag = False
+                break
+        if flag:
+            max_area = max(current_area, max_area)
+    return max_area
+
+
 if __name__ == "__main__":
     start_time = time.time()
     points = input_parser("data.txt")
     edges = get_all_edges(points)
     combinations = diagonal_red_tile_positions(points)
+    result = solver(combinations, edges)
     end_time = time.time()
+    print(result)
     print(f"Execution time: {end_time - start_time:.6f} seconds")
-
-
-# y=8
-# y=7     	 	.	.	.	.	.	#	#	#	#	#
-# y=6     	 	.	.	.	.	.	#	.	.	.	#
-# y=5     	 	#	#	#	#	#	#	.	.	.	#
-# y=4     	 	.	.	.	.	.	.	.		.	#
-# y=3     	 	#	#	#	#	#	#	#	#	.	#
-# y=2     	 	.	.	.	.	.	.	.	#	.	#
-# y=1     	 	.	.	.	.	.	.	.	#	#	#
-# y=0
-#        0	1	2	3	4	5	6	7	8	9	10	11	12	13
