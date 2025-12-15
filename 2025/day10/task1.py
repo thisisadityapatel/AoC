bf95fc24f393d16d84f0d8f7af0175233fffa81b
Aditya Patel <adityakdpatel@gmail.com>
2025-12-14 22:45:56 -0500

    comment

--- 2025/day10/task1.py
+++ 2025/day10/task1.py
@@ -22,7 +22,9 @@ def transform(data):
     return new_data
 
 
-def find_min(point):
+def find_min(
+    point,
+):  # DFS backtracking algorithm (optimized soliution uses BFS because we want to exist the first time we come across a solution).
     tracker = []
 
     def dfs(button_count, path_odd, prev_point, max_iter):
